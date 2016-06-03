b6c08da3c280a0e043ba551b4de7c75ecc9eba64
shawnmarie <shawn.abbott@versionone.com>
2016-06-03 14:46:02 -0400

    adding new files for manual testing

---
+++ Test/CreateRegressionActions.runnable.js
@@ -0,0 +1,172 @@
+var actions = require('./DevOps.Actions');
+var axios = require('axios');
+var v1sdk = require('v1sdk');
+var util = require('util');
+
+var dc = actions.DevopsCenter;
+var eh = actions.errorHandler;
+
+var namePipeline = "Pipeline for Regression by SMA";
+var nameTask = "Task for Regression by SMA";
+var nameWebhook = "Webhook for Regression by SMA";
+var nameCanvas = "Canvas for Regression by SMA";
+
+var descriptionPipeline = "This was created to test starting a pipeline.";
+var descriptionTask = "This was created to test starting a task.";
+var descriptionWebhook = "This was created to test hitting a webhook.";
+var descriptionCanvas = "This was created to test opening a canvas.";
+
+var assetType = dc.assetType;
+
+var triggerTypePipeline = 'TriggerType:240';
+var triggerTypeTask = 'TriggerType:241';
+var triggerTypeWebhook = 'TriggerType:242';
+var triggerTypeCanvas = 'TriggerType:243';
+
+var payloadPipeline = '{"definition":"Deploy to Staging","project":"test_project","group":"master"}';
+var payloadTask = '{"task":"Test_Task","initial_data":"{}"}';
+var payloadWebhook = '{"handler":"sma_test_webhook","smaStuff": "http://www.mocky.io/v2/571678901100005b2087da99"}';
+var payloadCanvas = '{"path": "demos/main/main.layout"}';
+
+var externalOid = 0;
+var triggerOid;
+
+
+function momentlessId(asset) {
+	id = asset.data.id;
+	var matches = id.match(/:(\d{4,}):/);
+	id = matches[1];
+	return id;
+}
+
+function typeAsset(asset) {
+	type = asset.data._type;
+	return type;
+}
+
+//Create an action for each asset type and trigger type combination
+
+//Project External Actions
+console.log("Create new action for the Project asset type with Trigger Type Pipeline");
+dc.CreateAction(namePipeline, 'Scope', triggerTypePipeline, payloadPipeline, descriptionPipeline).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Project asset type with Trigger Type Task");
+dc.CreateAction(nameTask, 'Scope', triggerTypeTask, payloadTask, descriptionTask).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Project asset type with Trigger Type Webhook");
+dc.CreateAction(nameWebhook, 'Scope', triggerTypeWebhook, payloadWebhook, descriptionWebhook).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Project asset type with Trigger Type Canvas");
+dc.CreateAction(nameCanvas, 'Scope', triggerTypeCanvas, payloadCanvas, descriptionCanvas).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+//Portfolio Item External Actions
+console.log("Create new action for the Portfolio Item asset type with Trigger Type Pipeline");
+dc.CreateAction(namePipeline, 'Epic', triggerTypePipeline, payloadPipeline, descriptionPipeline).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Portfolio Item asset type with Trigger Type Task");
+dc.CreateAction(nameTask, 'Epic', triggerTypeTask, payloadTask, descriptionTask).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Portfolio Item asset type with Trigger Type Webhook");
+dc.CreateAction(nameWebhook, 'Epic', triggerTypeWebhook, payloadWebhook, descriptionWebhook).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Portfolio Item asset type with Trigger Type Canvas");
+dc.CreateAction(nameCanvas, 'Epic', triggerTypeCanvas, payloadCanvas, descriptionCanvas).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+//Story External Actions
+console.log("Create new action for the Story asset type with Trigger Type Pipeline");
+dc.CreateAction(namePipeline, 'Story', triggerTypePipeline, payloadPipeline, descriptionPipeline).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Story asset type with Trigger Type Task");
+dc.CreateAction(nameTask, 'Story', triggerTypeTask, payloadTask, descriptionTask).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Story asset type with Trigger Type Webhook");
+dc.CreateAction(nameWebhook, 'Story', triggerTypeWebhook, payloadWebhook, descriptionWebhook).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Story asset type with Trigger Type Canvas");
+dc.CreateAction(nameCanvas, 'Story', triggerTypeCanvas, payloadCanvas, descriptionCanvas).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+//Defect External Actions
+console.log("Create new action for the Defect asset type with Trigger Type Pipeline");
+dc.CreateAction(namePipeline, 'Defect', triggerTypePipeline, payloadPipeline, descriptionPipeline).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Defect asset type with Trigger Type Task");
+dc.CreateAction(nameTask, 'Defect', triggerTypeTask, payloadTask, descriptionTask).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Defect asset type with Trigger Type Webhook");
+dc.CreateAction(nameWebhook, 'Defect', triggerTypeWebhook, payloadWebhook, descriptionWebhook).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the Defect asset type with Trigger Type Canvas");
+dc.CreateAction(nameCanvas, 'Defect', triggerTypeCanvas, payloadCanvas, descriptionCanvas).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+//TestSet External Actions
+console.log("Create new action for the TestSet asset type with Trigger Type Pipeline");
+dc.CreateAction(namePipeline, 'TestSet', triggerTypePipeline, payloadPipeline, descriptionPipeline).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the TestSet asset type with Trigger Type Task");
+dc.CreateAction(nameTask, 'TestSet', triggerTypeTask, payloadTask, descriptionTask).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the TestSet asset type with Trigger Type Webhook");
+dc.CreateAction(nameWebhook, 'TestSet', triggerTypeWebhook, payloadWebhook, descriptionWebhook).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
+
+console.log("Create new action for the TestSet asset type with Trigger Type Canvas");
+dc.CreateAction(nameCanvas, 'TestSet', triggerTypeCanvas, payloadCanvas, descriptionCanvas).then(function(asset) {
+	var id = momentlessId(asset);
+	console.log("\tCreated new ExternalAction: " + id);
+});
---
+++ Test/DevOps.Actions.runnable.js
@@ -0,0 +1,182 @@
+//(function() {
+var base = require('./lib/base');
+var axios = require('axios');
+var v1sdk = require('v1sdk');
+var util = require('util');
+
+var v1 = base.v1;
+var headerObj = base.headerObj;
+var username = base.username;
+var password = base.password;
+var protocol = base.protocol + '://';
+var baseUrl = protocol + base.hostname + '/' + base.instance;
+var devOpsCenterApiBaseUrl = baseUrl + '/DeliveryStream.mvc/';
+
+var DevopsCenter = {
+	CreateAction: function(name, assetType, triggerType, payload, description){
+		name = name || "Pipeline for Regression by SMA";
+		assetType = assetType || "Story";
+		payload = payload || '{"definition":"Deploy to Staging","project":"test_project","group":"master"}';
+		description = description || "This was created to test starting a pipeline.";
+		triggerType = triggerType || 'TriggerType:240';
+
+		//data returned is only the attributes of the post data
+		var data = {
+			"Name": name,
+			"ValidForAssetType": assetType,
+			"TriggerType": triggerType,
+			"Payload": payload,
+			"Description": description
+		};
+		return v1.create('ExternalAction', data);
+	},
+	DeleteAction: function(externalActionOid) {
+		var oidToken = 'ExternalAction:' + externalActionOid;
+		return v1.executeOperation(oidToken, 'Delete');
+	},
+	InvokeAction: function(externalActionOid, triggeringAssetOid, name, number, description, status) {
+		name = name || "Asset";
+		number = number || "A-00001";
+		description = description || null;
+		status = status || null;
+		
+		var url = devOpsCenterApiBaseUrl + util.format(
+			'ExternalActionInvoke?externalActionOid=ExternalAction:%d&triggeringAssetOid=%s',
+		 	externalActionOid, triggeringAssetOid);
+
+		console.log(url);
+		var data = {
+			"actionName":"ExternalAction-" + externalActionOid,
+			"id": externalActionOid,
+			"triggeringAssetData":{
+				"name": name,
+				"id": triggeringAssetOid,
+				"number": number,
+				"description": description,
+				"status": status
+			}
+		};
+
+		console.log("about to call post");
+		return axios.post(url, {}, {auth: {username: base.username, password: base.password}});
+	},
+	CreateActionInvocationByAssetAndAction: function(assetType, externalActionOid, triggeringAssetOid) {
+		assetType = assetType || 'Story';
+		externalActionOid = externalActionOid || 2243;
+		triggeringAssetOid = triggeringAssetOid || 1187;
+
+		var data = {
+			"InvokedOn": assetType + ":" + triggeringAssetOid,
+			"CausedBy": "ExternalAction:" + externalActionOid,
+			"Sent": ""
+		};
+		return v1.create('ExternalActionInvocation', data);		
+	},
+	GetCountOfExternalActionInvocationInvokedOnExternalAction: function(qType, relation, assetType, triggeringAssetOid) {
+		assetType = assetType || 'Story';
+		triggeringAssetOid = triggeringAssetOid || 1187;
+		var triggerAsset = assetType + ':' + triggeringAssetOid;
+
+		var url = baseUrl + util.format(
+				'/rest-1.v1/Data/ExternalActionInvocation?where=InvokedOn=%27triggerAsset%27', triggerAsset);
+
+		return axios.get(url);
+	},
+	ReadActionInvocationCausedByAction: function(externalActionOid) {
+		externalActionOid = externalActionOid || 2287;
+
+		var data = {
+			"from": "ExternalActionInvocation", 
+			"select": ["InvokedOn"], 
+			"where": {"CausedBy": "ExternalAction:" + externalActionOid}
+			};
+
+		return v1.query(data);
+	},
+	SelectExternalActionByValidForAssetType: function(assetType) {
+		assetType = assetType || 'Story';
+
+		var data = {
+			"from": "ExternalAction", 
+			"select": [],
+			"where": {"ValidForAssetType": assetType, "TriggerType.Name": "Webhook"}
+		};
+
+		return v1.query(data);
+	}
+};
+
+var errorHandler = function(error) {
+	console.log("in the error function");
+	console.log(error, error.data.exceptions);
+	//console.error(error);
+};
+
+var externalActionOid = 1222;
+var name;
+var number;
+var assetType = "Story";
+var triggerType;
+var triggeringAssetOid = 1184;
+var payload;
+var description;
+var status;
+
+
+/*DevopsCenter.CreateAction(name, assetType, triggerType, payload, description)
+.then(function(result) {
+	console.log(result);
+})
+.catch(errorHandler);*/
+
+
+var checkForData = function(result) {
+	console.log('checkForDAta', result.data[0].length === 0);
+	return result.data[0].length === 0 ? 
+	new Promise(function(resolve, reject){ reject("Error") }) : 
+	new Promise(function(resolve, reject){ resolve(result.data[0]) });
+}
+
+var getExternalActionOidToken = function(data) {
+	return new Promise(function(resolve, reject){ resolve(data[0]._oid) });
+}
+
+var invokeExternalAction = function(externalActionOidToken) {
+	new Promise(function(resolve, reject){ resolve(DevopsCenter.InvokeAction(externalActionOidToken, 'Story:1240')) });
+}
+
+var invokeActionSuccess = function(stuff) {
+	console.log('we invoked the action')
+}
+
+DevopsCenter.SelectExternalActionByValidForAssetType(assetType)
+.then(checkForData)
+.then(getExternalActionOidToken)
+.then(invokeExternalAction)
+.then(invokeActionSuccess);
+// console.log('Protocol: ' + protocol);
+// console.log('BaseUrl: ' + baseUrl);
+// DevopsCenter.InvokeAction(externalActionOid, 'Epic:1205')
+// .then(function(result) {
+// 	console.log("before the result");
+// 	console.log(result);
+// })
+// .catch(errorHandler);
+
+
+// DevopsCenter.DeleteAction(externalActionOid)
+// .then(function(result) {
+// 	console.log(result);
+// })
+// .catch(errorHandler);
+
+/*module.exports = {
+	DevopsCenter: DevopsCenter,
+	errorHandler: errorHandler
+};
+}());*/
+
+// [
+// 	[_oid: value],
+// 	[_oid: value]
+// ]
